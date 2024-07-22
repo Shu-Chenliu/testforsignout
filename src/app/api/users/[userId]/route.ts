8f2b470dc12e8ae00fb541558f86c28f4a3565d8
susan <h811124@nehs.hc.edu.tw>
2024-07-22 19:41:32 +0800

    fixbug

--- src/app/api/users/[userId]/route.ts
+++ src/app/api/users/[userId]/route.ts
@@ -1,49 +1,49 @@
-// import { NextRequest, NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
-// import {  eq } from "drizzle-orm";
+import {  eq } from "drizzle-orm";
 
-// import { db } from "@/db";
-// import { usersTable } from "@/db/schema";
+import { db } from "@/db";
+import { usersTable } from "@/db/schema";
 
-// export async function GET(
-//   // eslint-disable-next-line
-//   req: NextRequest,
-//     {
-//       params,
-//     }: {
-//       params: {
-//         userId: number;
-//       };
-//     },) {
-//     try {
-//       const usersStatus = await db
-//         .select({
-//           id: usersTable.id,
-//           authority: usersTable.authority,
-//         })
-//         .from(usersTable)
-//         .where(
-//             eq(usersTable.id, params.userId)
-//         );
+export async function GET(
+  // eslint-disable-next-line
+  req: NextRequest,
+    {
+      params,
+    }: {
+      params: {
+        userId: number;
+      };
+    },) {
+    try {
+      const usersStatus = await db
+        .select({
+          id: usersTable.id,
+          authority: usersTable.authority,
+        })
+        .from(usersTable)
+        .where(
+            eq(usersTable.id, params.userId)
+        );
       
-//       if (!usersStatus) {
-//         return NextResponse.json({ error: `no idp status available` }, { status: 404 });
-//       }
+      if (!usersStatus) {
+        return NextResponse.json({ error: `no idp status available` }, { status: 404 });
+      }
   
-//       return NextResponse.json(
-//         {
-//           usersStatus: usersStatus,
-//         },
-//         { status: 200 },
-//       );
-//     } catch (error) {
-//       return NextResponse.json(
-//         {
-//           error: "Internal Server Error",
-//         },
-//         {
-//           status: 500,
-//         },
-//       );
-//     }
-//   }
+      return NextResponse.json(
+        {
+          usersStatus: usersStatus,
+        },
+        { status: 200 },
+      );
+    } catch (error) {
+      return NextResponse.json(
+        {
+          error: "Internal Server Error",
+        },
+        {
+          status: 500,
+        },
+      );
+    }
+  }
